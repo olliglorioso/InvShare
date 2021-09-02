423f1d5875571c336a357c37bad79c88cce62a32
olliglorioso <olli.glorioso@gmail.com>
2021-09-02 16:46:30 +0300

    pipeline

--- backend/index.ts
+++ backend/index.ts
@@ -61,7 +61,7 @@ const startServer = async () => {
     server.applyMiddleware({app})
     void app.listen(({port: process.env.PORT}), () => {
         console.log(`Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`);
-        console.log("Server works")
+        console.log("Server works!")
     });
     return app
 }
