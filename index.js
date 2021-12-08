9a96efa5647df0676c9695ce4be3a14b942ec0d5
olliglorioso <olli.glorioso@gmail.com>
2021-12-08 22:18:47 +0200

    pipeline

--- index.js
+++ index.js
@@ -14,57 +14,87 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.appi = void 0;
 const apollo_server_express_1 = require("apollo-server-express");
-const graphql_1 = require("graphql");
 const mongoose_1 = __importDefault(require("mongoose"));
 const user_1 = __importDefault(require("./src/models/user"));
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-var-requires
-require('dotenv').config();
+require("dotenv").config();
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-const resolvers_1 = __importDefault(require("./src/resolvers"));
-const typeDefs_1 = require("./src/typeDefs");
+const resolvers_1 = __importDefault(require("./src/resolvers/resolvers"));
+const typeDefs_1 = require("./src/utils/typeDefs");
 const express_1 = __importDefault(require("express"));
-const uuid_1 = require("uuid");
 const cors_1 = __importDefault(require("cors"));
+const http_1 = require("http");
+const subscriptions_transport_ws_1 = require("subscriptions-transport-ws");
+const graphql_1 = require("graphql");
+const schema_1 = require("@graphql-tools/schema");
+const connect_history_api_fallback_1 = __importDefault(require("connect-history-api-fallback"));
+// In this index.ts-file are all the configurations and the server will be started here.
 const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
-    const MONGODB_URI = process.env.NODE_ENV === 'test'
-        ? process.env.MONGODB_TEST_URI || ''
-        : process.env.MONGODB_URI || '';
-    mongoose_1.default.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-        .then(() => {
-        console.log('connected to mongodb');
-    })
-        .catch((error) => {
-        console.log('error connection to mongodb ', error);
+    // Choosing the endpoint for the database with enviroment variables.
+    const MONGODB_URI = process.env.NODE_ENV === "test"
+        ? process.env.MONGODB_TEST_URI || ""
+        : process.env.MONGODB_URI || "";
+    // Connecting to the database.
+    void mongoose_1.default.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+    // Creating the express-server.
+    const app = express_1.default();
+    // Adding cors-middleware.
+    app.use(cors_1.default());
+    // Adding the history-api-fallback-middleware.
+    app.use(connect_history_api_fallback_1.default());
+    // Adding the express.static-middleware.
+    app.use(express_1.default.static("build"));
+    // Creating endpoint for healthcheck.
+    app.get("/healthcheck", (_req, res) => {
+        res.send("toimii");
     });
-    const server = new apollo_server_express_1.ApolloServer({
+    const httpServer = http_1.createServer(app);
+    const schema = schema_1.makeExecutableSchema({
         typeDefs: typeDefs_1.typeDefs,
         resolvers: resolvers_1.default,
+    });
+    // Creating the apollo-server.
+    const server = new apollo_server_express_1.ApolloServer({
+        schema,
         context: ({ req }) => __awaiter(void 0, void 0, void 0, function* () {
             const auth = req ? req.headers.authorization : null;
-            if (auth && auth.toLowerCase().startsWith('bearer ')) {
+            if (auth && auth.toLowerCase().startsWith("bearer ")) {
                 const decodedToken = jsonwebtoken_1.default.verify(auth.substring(7), process.env.SECRETFORTOKEN);
-                const currentUser = yield user_1.default
-                    .findById(decodedToken.id).populate('usersHoldings').populate('usersTransactions');
+                const currentUser = yield user_1.default.findById(decodedToken.id)
+                    .populate({ path: "usersHoldings", populate: { path: "usersStock" } })
+                    .populate({ path: "usersTransactions", populate: { path: "transactionStock" } })
+                    .populate({ path: "usersFollowing", populate: { path: "user", populate: { path: "usersTransactions", populate: { path: "transactionStock" } } } })
+                    .populate({ path: "usersFollowers", populate: { path: "user" } });
                 return { currentUser };
             }
             return null;
         }),
         introspection: true,
-        formatError: (error) => {
-            const errId = uuid_1.v4();
-            console.log(errId);
-            console.log(error);
-            return new graphql_1.GraphQLError(`Error occured: ${errId}`);
-        }
+        plugins: [
+            {
+                serverWillStart() {
+                    return __awaiter(this, void 0, void 0, function* () {
+                        return {
+                            drainServer() {
+                                return __awaiter(this, void 0, void 0, function* () {
+                                    subscriptionServer.close();
+                                });
+                            }
+                        };
+                    });
+                }
+            }
+        ],
     });
-    const app = express_1.default();
-    app.use(cors_1.default());
-    app.use(express_1.default.static('build'));
+    const subscriptionServer = subscriptions_transport_ws_1.SubscriptionServer.create({ schema, execute: graphql_1.execute, subscribe: graphql_1.subscribe,
+        onConnect: () => console.log("connected"),
+        onDisconnect: () => console.log("disconnected")
+    }, { server: httpServer, path: "/subscriptions" });
+    // Starting the server.
     void (yield server.start());
     server.applyMiddleware({ app });
-    void app.listen(({ port: process.env.PORT }), () => {
+    void httpServer.listen(({ port: process.env.PORT }), () => {
         console.log(`Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`);
-        console.log("Server works");
     });
     return app;
 });
