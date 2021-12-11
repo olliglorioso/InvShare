1cfc466a585cdc3fbc58265a6ec613d7c38c7943
olliglorioso <olli.glorioso@gmail.com>
2021-12-11 21:38:16 +0200

    pipeline

---
+++ frontend/src/components/BuyStocksRoute/MainChart.test.js
@@ -0,0 +1,12 @@
+// import React from "react";
+// import "@testing-library/jest-dom/extend-expect";
+// import { render } from "react-dom";
+// import MainChart from "./MainChart";
+
+// test("MainChart renders correctly", () => {
+//     const symbol = "AAPL";
+
+//     const component = render(<MainChart stock={symbol} />);
+
+//     expect(component.container).toHaveTextContent(symbol);
+// });
